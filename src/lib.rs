1224e2ea82bc49b7fb63b97c024b9a04c78b2829
Peter McEvoy <peter@tlon.io>
2021-12-14 11:58:14 -0700

    Set up scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+enum Loobean {
+    Yes,
+    No,
+}
+
+trait Noun {}
+
+struct Atom(u32);
+
+struct Cell {
+    head: Box<dyn Noun>,
+    tail: Box<dyn Noun>,
+}
+
+// ?
+trait Wut {
+    fn wut(&self) {}
+}
+
+// +
+trait Lus {
+    fn lus(&self) {}
+}
+
+// =
+trait Tis {
+    fn tis(&self) {}
+}
+
+// /
+trait Fas {
+    fn fas(&self) {}
+}
+
+// #
+trait Hax {
+    fn hax(&self) {}
+}
+
+// *
+trait Tar {
+    fn tar(&self) {}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
