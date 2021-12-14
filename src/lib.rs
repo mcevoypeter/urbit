2847ea9bea39483a0a8d6249e7636822966bfeb1
Peter McEvoy <peter@tlon.io>
2021-12-14 15:01:44 -0700

    Implement PartialEq for Atom, Cell, and Noun

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ enum Noun {
     Cell(Cell),
 }
 
+#[derive(PartialEq)]
 struct Atom(u64);
 
 struct Cell {
@@ -47,6 +48,20 @@ trait Tar {
     fn tar(&self) {}
 }
 
+impl PartialEq for Noun {
+    fn eq(&self, other: &Self) -> bool {
+        if let (Noun::Atom(lh), Noun::Atom(rh)) = (&*self, &*other) {
+            return lh == rh
+        }
+
+        if let (Noun::Cell(lh), Noun::Cell(rh)) = (&*self, &*other) {
+            return lh == rh
+        }
+
+        false
+    }
+}
+
 impl Wut for Atom {
     fn wut(&self) -> Loobean {
         Loobean::No
@@ -56,6 +71,34 @@ impl Wut for Atom {
 impl Lus for Atom {
     fn lus(self) -> Atom {
         Atom(1 + self.0)
+    }
+}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+
+        // The heads of both self and other are atoms.
+        if let (Noun::Atom(lh_head), Noun::Atom(rh_head))
+            = (&*self.head, &*other.head)
+        {
+            if lh_head != rh_head {
+                return false
+            }
+            return *self.tail == *other.tail
+        }
+
+        // The heads of both self and other are cells.
+        if let (Noun::Cell(lh_head), Noun::Cell(rh_head))
+            = (&*self.head, &*other.head)
+        {
+            if !Self::eq(lh_head, rh_head) {
+                return false
+            }
+
+            return *self.tail == *other.tail
+        }
+
+        false
     }
 }
 
@@ -104,5 +147,133 @@ mod tests {
         // +999 -> 1000
         let atom = Atom(999);
         assert_eq!(1000, atom.lus().0);
+    }
+
+    #[test]
+    fn partialeq_cell() {
+        // [71 109] == [71 109]
+        let lh = Cell {
+            head: Box::new(Noun::Atom(Atom(71))),
+            tail: Box::new(Noun::Atom(Atom(109))),
+        };
+        let rh = Cell {
+            head: Box::new(Noun::Atom(Atom(71))),
+            tail: Box::new(Noun::Atom(Atom(109))),
+        };
+        assert!(lh == rh);
+
+        // [71 109] != [109 71]
+        let lh = Cell {
+            head: Box::new(Noun::Atom(Atom(71))),
+            tail: Box::new(Noun::Atom(Atom(109))),
+        };
+        let rh = Cell {
+            head: Box::new(Noun::Atom(Atom(109))),
+            tail: Box::new(Noun::Atom(Atom(71))),
+        };
+        assert!(lh != rh);
+
+        // [11 [12 13]] == [11 [12 13]]
+        let lh = Cell {
+            head: Box::new(Noun::Atom(Atom(11))),
+            tail: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(12))),
+                tail: Box::new(Noun::Atom(Atom(13))),
+            })),
+        };
+        let rh = Cell {
+            head: Box::new(Noun::Atom(Atom(11))),
+            tail: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(12))),
+                tail: Box::new(Noun::Atom(Atom(13))),
+            })),
+        };
+        assert!(lh == rh);
+
+        // [11 [12 13]] != [11 [13 12]]
+        let lh = Cell {
+            head: Box::new(Noun::Atom(Atom(11))),
+            tail: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(12))),
+                tail: Box::new(Noun::Atom(Atom(13))),
+            })),
+        };
+        let rh = Cell {
+            head: Box::new(Noun::Atom(Atom(11))),
+            tail: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(13))),
+                tail: Box::new(Noun::Atom(Atom(12))),
+            })),
+        };
+        assert!(lh != rh);
+    }
+
+    #[test]
+    fn partialeq_noun() {
+        // 500 == 500
+        let lh = Noun::Atom(Atom(500));
+        let rh = Noun::Atom(Atom(500));
+        assert!(lh == rh);
+
+        // 499 != 501
+        let lh = Noun::Atom(Atom(499));
+        let rh = Noun::Atom(Atom(501));
+        assert!(lh != rh);
+
+        // [0 5] == [0 5]
+        let lh = Noun::Cell(Cell {
+            head: Box::new(Noun::Atom(Atom(0))),
+            tail: Box::new(Noun::Atom(Atom(5))),
+        });
+        let rh = Noun::Cell(Cell {
+            head: Box::new(Noun::Atom(Atom(0))),
+            tail: Box::new(Noun::Atom(Atom(5))),
+        });
+        assert!(lh == rh);
+
+        // [0 0] == [0 5]
+        let lh = Noun::Cell(Cell {
+            head: Box::new(Noun::Atom(Atom(0))),
+            tail: Box::new(Noun::Atom(Atom(0))),
+        });
+        let rh = Noun::Cell(Cell {
+            head: Box::new(Noun::Atom(Atom(0))),
+            tail: Box::new(Noun::Atom(Atom(5))),
+        });
+        assert!(lh != rh);
+
+        // [[44 22] 88] == [[44 22] 88]
+        let lh = Noun::Cell(Cell {
+            head: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(44))),
+                tail: Box::new(Noun::Atom(Atom(22))),
+            })),
+            tail: Box::new(Noun::Atom(Atom(88))),
+        });
+        let rh = Noun::Cell(Cell {
+            head: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(44))),
+                tail: Box::new(Noun::Atom(Atom(22))),
+            })),
+            tail: Box::new(Noun::Atom(Atom(88))),
+        });
+        assert!(lh == rh);
+
+        // [[44 22] 88] != [44 [22 88]]
+        let lh = Noun::Cell(Cell {
+            head: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(44))),
+                tail: Box::new(Noun::Atom(Atom(22))),
+            })),
+            tail: Box::new(Noun::Atom(Atom(88))),
+        });
+        let rh = Noun::Cell(Cell {
+            head: Box::new(Noun::Atom(Atom(44))),
+            tail: Box::new(Noun::Cell(Cell {
+                head: Box::new(Noun::Atom(Atom(22))),
+                tail: Box::new(Noun::Atom(Atom(88))),
+            })),
+        });
+        assert!(lh != rh);
     }
 }
