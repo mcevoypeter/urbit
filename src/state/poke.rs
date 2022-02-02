4aff64e6c26225089ce2bf918adbe650d769a467
Peter McEvoy <peter@tlon.io>
2022-02-01 17:44:17 -0800

    Implement Evt for PokeRequest

--- src/state/poke.rs
+++ src/state/poke.rs
@@ -1,19 +1,57 @@
+use std::cmp::Ordering;
+
 use nock::{Cell, Noun};
 
 use crate::{
-    event_log::EventLog,
+    event_log::{EventLog, Evt},
     kernel::Kernel,
     state::{Req, Response, StagedResp},
 };
 
 /// Write request.
-struct PokeRequest(Cell);
+struct PokeRequest {
+    id: u64,
+    req: Cell,
+}
+
+impl Eq for PokeRequest {}
+
+impl Evt for PokeRequest {
+    type Id = u64;
+    type Request = Cell;
+
+    fn _id(&self) -> &Self::Id {
+        &self.id
+    }
+
+    fn _request(&self) -> &Self::Request {
+        &self.req
+    }
+}
+
+impl Ord for PokeRequest {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.id.cmp(&other.id)
+    }
+}
+
+impl PartialEq for PokeRequest {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl PartialOrd for PokeRequest {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 impl Req for PokeRequest {
     type Output = PokeResponse;
 
     fn request(&self) -> &Cell {
-        &self.0
+        &self.req
     }
 
     fn evaluate(self, _arvo: Kernel) -> (Self::Output, Kernel) {
