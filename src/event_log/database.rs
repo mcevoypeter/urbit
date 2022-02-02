43d4c30b28f70d0f9c1d43b4f6047e7454c0641c
Peter McEvoy <peter@tlon.io>
2022-02-02 13:59:23 -0800

    Add environment field to Lmdb struct

--- src/event_log/database.rs
+++ src/event_log/database.rs
@@ -1,23 +1,27 @@
+use lmdb::Environment;
+
 use serde::{de::DeserializeOwned, Serialize};
 
 pub trait KeyValStore {
     type Val: DeserializeOwned + Serialize;
 
-    fn read(key: u64) -> Result<Self::Val, ()>;
+    fn read(&self, key: u64) -> Result<Self::Val, ()>;
 
-    fn write(key: u64, val: Self::Val) -> Result<Option<Self::Val>, ()>;
+    fn write(&self, key: u64, val: Self::Val) -> Result<Option<Self::Val>, ()>;
 }
 
-pub struct Lmdb;
+pub struct Lmdb {
+    _env: Environment,
+}
 
 impl KeyValStore for Lmdb {
     type Val = Vec<u8>;
 
-    fn read(key: u64) -> Result<Self::Val, ()> {
+    fn read(&self, key: u64) -> Result<Self::Val, ()> {
         unimplemented!("{}", key)
     }
 
-    fn write(key: u64, val: Self::Val) -> Result<Option<Self::Val>, ()> {
+    fn write(&self, key: u64, val: Self::Val) -> Result<Option<Self::Val>, ()> {
         unimplemented!("{} {:?}", key, val)
     }
 }
