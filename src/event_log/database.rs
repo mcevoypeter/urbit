2f32902d1e00ed24b829011e75dd011a8920b521
Peter McEvoy <peter@tlon.io>
2022-02-02 14:03:58 -0800

    Add KeyValStore::new() method

--- src/event_log/database.rs
+++ src/event_log/database.rs
@@ -1,9 +1,13 @@
+use std::path::Path;
+
 use lmdb::Environment;
 
 use serde::{de::DeserializeOwned, Serialize};
 
 pub trait KeyValStore {
     type Val: DeserializeOwned + Serialize;
+
+    fn new(path: &Path) -> Self;
 
     fn read(&self, key: u64) -> Result<Self::Val, ()>;
 
@@ -16,6 +20,10 @@ pub struct Lmdb {
 
 impl KeyValStore for Lmdb {
     type Val = Vec<u8>;
+
+    fn new(path: &Path) -> Self {
+        unimplemented!("{}", path.display())
+    }
 
     fn read(&self, key: u64) -> Result<Self::Val, ()> {
         unimplemented!("{}", key)
