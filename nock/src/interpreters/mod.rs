25385a47490e251d214fb61a2fbeaa0f83cc0a7f
Peter McEvoy <peter@tlon.io>
2022-02-23 14:12:54 -0800

    nock: move operator traits into interpreters module

--- nock/src/interpreters/mod.rs
+++ nock/src/interpreters/mod.rs
@@ -1,4 +1,7 @@
-use super::*;
+use crate::{
+    a, atom::Atom, b, bad_literal, c, cell::Cell, ch, ct, error::Error, loobean::Loobean, na, nc,
+    noun::Noun, unexpected_atom, unexpected_cell, unexpected_iatom,
+};
 
 #[cfg(feature = "iterative_tree")]
 mod iterative_tree;
@@ -9,6 +12,59 @@ mod recursive_tree;
 compile_error!(
     "feature \"iterative_tree\" and \"recursive_tree\" cannot be enabled at the same time"
 );
+
+/// The ? Nock operator.
+///
+/// Determine if a noun is a cell or an atom.
+///
+/// ```console
+/// ?[x] -> 0 if x is a cell
+/// ?[x] -> 1 if x is an atom
+/// ```
+trait Wut {
+    fn wut(&self) -> Loobean;
+}
+
+/// The + Nock operator.
+///
+/// Increment an atom.
+///
+/// ```console
+/// +[x] -> 1 + x   if x is an atom
+/// +[x] -> *crash* if x is a cell
+/// ```
+trait Lus {
+    fn lus(self) -> Atom;
+}
+
+/// The = Nock Operator.
+///
+/// Determine if two nouns are equal.
+///
+/// ```console
+/// =[x y] -> 0 if x and y are the same noun
+/// =[x y] -> 1 otherwise
+/// ```
+trait Tis {
+    fn tis(&self) -> Loobean;
+}
+
+/// The / Nock operator.
+trait Fas {
+    fn fas(self) -> Result<Noun, Error>;
+}
+
+/// The # Nock operator.
+trait Hax {
+    fn hax(self) -> Result<Noun, Error>;
+}
+
+/// The * Nock operator.
+///
+/// Apply the Nock interpreter function.
+pub trait Tar {
+    fn tar(self) -> Result<Noun, Error>;
+}
 
 impl Wut for Atom {
     fn wut(&self) -> Loobean {
--- nock/src/lib.rs
+++ nock/src/lib.rs
@@ -1,5 +1,3 @@
-use crate::{atom::Atom, cell::Cell, error::Error, loobean::Loobean, noun::Noun};
-
 #[macro_use]
 pub mod atom;
 #[macro_use]
@@ -8,59 +6,6 @@ pub mod error;
 pub mod interpreters;
 pub mod loobean;
 pub mod noun;
-
-/// The ? Nock operator.
-///
-/// Determine if a noun is a cell or an atom.
-///
-/// ```console
-/// ?[x] -> 0 if x is a cell
-/// ?[x] -> 1 if x is an atom
-/// ```
-trait Wut {
-    fn wut(&self) -> Loobean;
-}
-
-/// The + Nock operator.
-///
-/// Increment an atom.
-///
-/// ```console
-/// +[x] -> 1 + x   if x is an atom
-/// +[x] -> *crash* if x is a cell
-/// ```
-trait Lus {
-    fn lus(self) -> Atom;
-}
-
-/// The = Nock Operator.
-///
-/// Determine if two nouns are equal.
-///
-/// ```console
-/// =[x y] -> 0 if x and y are the same noun
-/// =[x y] -> 1 otherwise
-/// ```
-trait Tis {
-    fn tis(&self) -> Loobean;
-}
-
-/// The / Nock operator.
-trait Fas {
-    fn fas(self) -> Result<Noun, Error>;
-}
-
-/// The # Nock operator.
-trait Hax {
-    fn hax(self) -> Result<Noun, Error>;
-}
-
-/// The * Nock operator.
-///
-/// Apply the Nock interpreter function.
-pub trait Tar {
-    fn tar(self) -> Result<Noun, Error>;
-}
 
 /// Box::new($e)
 #[macro_export]
